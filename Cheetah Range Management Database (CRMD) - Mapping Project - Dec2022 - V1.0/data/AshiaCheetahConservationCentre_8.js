bddd3f7bc0f2c8dbe9663ca9ec75b05e899daa69
VinceNaude <119862861+VinceNaude@users.noreply.github.com>
2022-12-05 14:37:30 +0200

    Add files via upload

---
+++ Cheetah Range Management Database (CRMD) - Mapping Project - Dec2022 - V1.0/data/AshiaCheetahConservationCentre_8.js
@@ -0,0 +1,1 @@
+var json_AshiaCheetahConservationCentre_8 = {"type":"FeatureCollection","name":"AshiaCheetahConservationCentre_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ Cheetah Range Management Database (CRMD) - Mapping Project - Dec2022 - V1.0/data/BuffalokloofPrivateGameReserve_7.js
@@ -0,0 +1,1 @@
+var json_BuffalokloofPrivateGameReserve_7 = {"type":"FeatureCollection","name":"BuffalokloofPrivateGameReserve_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
